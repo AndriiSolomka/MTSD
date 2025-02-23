88433724bbb64d699edae3a81b3f86de974b13d0
Solomka Andrii <andriissolomka@gmail.com>
2025-02-23 20:56:43 +0200

    Refactor quadratic equation solver

--- lab_1/utils/quadraticEquation.js
+++ lab_1/utils/quadraticEquation.js
@@ -1,27 +1,25 @@
 "use strict";
 
-const roots = {
-  x1: null,
-  x2: null,
-};
-
 const calculateEquation = ({ a, b, c }) => {
-  if (a === 0) return false;
+  if (a === 0) console.error('Coefficient "a" couldn`t be 0');
+  const D = b * b - 4 * a * c;
 
-  const d = b * b - 4 * a * c;
-
-  if (d > 0) {
-    roots.x1 = (-b - Math.sqrt(d)) / (2 * a);
-    roots.x2 = (-b + Math.sqrt(d)) / (2 * a);
-    return `There are 2 roots\n x1 = ${roots.x1}\n x2 = ${roots.x2}`;
+  if (D > 0) {
+    return {
+      x1: (-b - Math.sqrt(D)) / (2 * a),
+      x2: (-b + Math.sqrt(D)) / (2 * a),
+      message: "There are 2 roots",
+    };
   }
 
-  if (d === 0) {
-    roots.x1 = -b / (2 * a);
-    return `There are 1 roots\n x1 = ${roots.x1}`;
+  if (D === 0) {
+    return {
+      x1: -b / (2 * a),
+      message: "There are 1 roots",
+    };
   }
 
-  if (d < 0) return `There are 0 roots`;
+  return { message: `There are 0 roots` };
 };
 
 export { calculateEquation };
