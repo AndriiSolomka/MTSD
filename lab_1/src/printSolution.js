4d32ef316bc83e5b3208339e9abfda40ba5f7f82
Solomka Andrii <andriissolomka@gmail.com>
2025-02-24 15:39:21 +0200

    Delete printSolution.js

--- lab_1/src/printSolution.js
+++
@@ -1,9 +0,0 @@
-const printSolution = (result) => {
-  console.log(result);
-};
-
-function printEquation({ a, b, c }) {
-  console.log(`Equation is: (${a}) x^2 + (${b}) x + (${c}) = 0`);
-}
-
-export { printSolution, printEquation };
