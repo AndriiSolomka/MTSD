b389d5fadef75c03b57f20d8332636394c9e9a48
Solomka Andrii <andriissolomka@gmail.com>
2025-02-24 17:19:06 +0200

    Add support for printing roots x1, x2 in printSolution function

--- lab_1/src/print.js
+++ lab_1/src/print.js
@@ -1,5 +1,8 @@
 const printSolution = (result) => {
-  console.log(result);
+  console.log(result.message);
+
+  if ("x1" in result) console.log(`x1 = ${result.x1}`);
+  if ("x2" in result) console.log(`x2 = ${result.x2}`);
 };
 
 function printEquation({ a, b, c }) {
