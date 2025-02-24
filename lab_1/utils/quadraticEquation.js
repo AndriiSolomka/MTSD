01aff3c2b5ecba9721e736fca56ba05c0ef4d133
Solomka Andrii <andriissolomka@gmail.com>
2025-02-24 15:44:59 +0200

    Fix: exit process when coefficient "a" is 0

--- lab_1/utils/quadraticEquation.js
+++ lab_1/utils/quadraticEquation.js
@@ -1,7 +1,10 @@
 "use strict";
 
 const calculateEquation = ({ a, b, c }) => {
-  if (a === 0) console.error('Coefficient "a" couldn`t be 0');
+  if (a === 0) {
+    console.error('Coefficient "a" couldn`t be 0');
+    process.exit(1);
+  }
   const D = b * b - 4 * a * c;
 
   if (D > 0) {
